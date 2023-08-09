882ca90f45ac2227dee06b113f385dc82cfa537c
vule2303 <leductuanvu5@gmail.com>
2023-08-09 21:49:18 +0700

    add _layout + home

---
+++ Controllers/HomeController1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Core.Controllers
+{
+    public class HomeController1 : Controller
+    {
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method.... ";
+        }
+    }
+}
--- Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SeconHand_BE.MvcApplication" Language="C#" %>
