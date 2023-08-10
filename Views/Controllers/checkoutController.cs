653b7e34912595d02c0f6f3267637c0530212a0a
ThaiMC <93109563+goddeath008@users.noreply.github.com>
2023-08-10 09:42:50 +0700

    Trangthanhtoan (#4)
    add checkout view

---
+++ Views/Controllers/checkoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Core.Controllers
+{
+    public class checkoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("/Views/checkout/checkout.cshtml");
+        }
+    }
+}
