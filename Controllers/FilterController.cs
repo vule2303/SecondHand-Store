e4f608ca9a57ee8c7aa6492895279c54b4d76031
your name <congthaifc01@gmail.com>
2023-08-10 10:04:03 +0700

    Filter

---
+++ Controllers/FilterController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Core.Controllers
+{
+    public class FilterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("/Views/Filter/Filter.cshtml");
+        }
+    }
+}
