fcd40c7f41a3e44b598ef2b8fe48f8026635e0b9
Vũ <107258990+vule2303@users.noreply.github.com>
2023-08-14 10:35:38 +0700

    19 contact (#21)
    * add view contact

---
+++ Controllers/Client/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Core.Controllers.Client
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("/Views/Client/Contact/Index.cshtml");
+        }
+    }
+}
