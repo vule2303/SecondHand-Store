e3355fa343d941bf91406dc663eb10fa921e3946
Vũ <107258990+vule2303@users.noreply.github.com>
2023-09-08 10:34:18 +0700

    49 add category crud (#51)
    * update CRUD-category

--- SecondHand.Web/Controllers/Server/CategoryController.cs
+++ SecondHand.Web/Controllers/Server/CategoryController.cs
@@ -22,12 +22,8 @@ namespace MVC_Core.Controllers.Server
             return View(cateList);
         }
         public IActionResult Create() 
-        {
-            var parentCategory = _context.Categories
-                .Where(x => x.ParentId == null)
-                .ToList();
 
-            ViewBag.ParentCategories = parentCategory;
+        {          
 
             return View();
         }
@@ -39,10 +35,80 @@ namespace MVC_Core.Controllers.Server
                 _context.Categories.Add(obj);
                 _context.SaveChanges();
 
+                TempData["Success"] = "Tạo mới thành công";
                 return RedirectToAction("Index");
             }
-            
+            TempData["Error"] = "Tạo mới không thành công";
             return View();
+        }
+        public IActionResult Update(int? id)
+        {
+
+
+            if(id == null || id ==0)
+            {
+                return NotFound();
+            }
+            Category categoryFromDb = _context.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+
+            }
+
+            
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Update(Category obj)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Update(obj);
+                _context.SaveChanges();
+                TempData["Success"] = "Cập nhật thành công";
+
+                return RedirectToAction("Index");
+            }
+            TempData["Error"] = "Cập nhật không thành công";
+
+            return View();
+        }
+        public IActionResult Delete(int? id)
+        {
+
+
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category categoryFromDb = _context.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+
+            }
+
+          
+
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Delete(Category obj)
+        {
+            obj = _context.Categories.Find(obj.Id);
+            if (obj == null)
+            {
+                TempData["Error"] = "Xoá không thành công";
+                return NotFound();
+            }
+            _context.Categories.Remove(obj);
+
+            _context.SaveChanges();
+            TempData["Success"] = "Xoá thành công";
+            return RedirectToAction("Index");
+         
         }
     }
 }
