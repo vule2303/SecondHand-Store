645c52003a97701b4458d96822bc36f42acceb99
your name <congthaifc01@gmail.com>
2023-09-11 09:14:02 +0700

    Contact

--- SecondHand.Web/Controllers/Client/ContactController.cs
+++ SecondHand.Web/Controllers/Client/ContactController.cs
@@ -1,12 +1,45 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.DotNet.Scaffolding.Shared.Messaging;
+using SecondHand.DataAccess.Data;
+using SecondHand.Models.Domain;
 
-namespace SecondHand.Controllers.Client
+namespace MVC_Core.Controllers.Client
 {
     public class ContactController : Controller
     {
+        private readonly S2HandDbContext _context;
+
+        public ContactController(S2HandDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View("/Views/Client/Contact/Index.cshtml");
+            List<Contact> contact = _context.Contacts.ToList();
+            return View(contact);
+        }
+        public ActionResult Create()
+        {
+            var model = new Contact();
+            return View(model);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Contacts.Add(contact);
+                _context.SaveChanges();
+
+                return RedirectToAction("Announce");
+            }
+            return View(contact);
+
+        }
+        public ActionResult Announce()
+        {
+            return View("/Views/Contact/Announce.cshtml");
         }
     }
 }
