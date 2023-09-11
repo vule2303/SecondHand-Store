3ff27f36fd1c6450b0bc39b251887934c4c00677
phu01072003 <119121651+phu01072003@users.noreply.github.com>
2023-09-11 16:03:18 +0700

    Crud Order (#56)

---
+++ SecondHand.Web/Controllers/Server/OrdersController.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SecondHand.DataAccess.Data;
+using SecondHand.Models.Domain;
+
+namespace MVC_Core.Controllers.Server
+{
+    public class OrdersController : Controller
+    {
+        private readonly S2HandDbContext _context;
+
+        public OrdersController(S2HandDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Orders
+        public async Task<IActionResult> Index()
+        {
+            var s2HandDbContext = _context.Orders.Include(o => o.Promotion).Include(o => o.User);
+            return View(await s2HandDbContext.ToListAsync());
+        }
+
+        // GET: Orders/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Orders == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Orders
+                .Include(o => o.Promotion)
+                .Include(o => o.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return View(order);
+        }
+
+        // GET: Orders/Create
+        public IActionResult Create()
+        {
+            ViewData["PromotionId"] = new SelectList(_context.Promotions, "Id", "Code");
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: Orders/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,UserId,PromotionId,PaymentStatus,PaymentMethod,OrderStatus,Subtotal,Disscount,Total,Note,Created,IsCancelled")] Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(order);
+                await _context.SaveChangesAsync();
+                TempData["Success"] = "Tạo mới thành công";
+                return RedirectToAction("Index");
+            }
+            ViewData["PromotionId"] = new SelectList(_context.Promotions, "Id", "Code", order.PromotionId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", order.UserId);
+            TempData["Error"] = "Tạo mới không thành công";
+            return View(order);
+        }
+
+        // GET: Orders/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Orders == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Orders.FindAsync(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+            ViewData["PromotionId"] = new SelectList(_context.Promotions, "Id", "Code", order.PromotionId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", order.UserId);
+            return View(order);
+        }
+
+        // POST: Orders/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,UserId,PromotionId,PaymentStatus,PaymentMethod,OrderStatus,Subtotal,Disscount,Total,Note,Created,IsCancelled")] Order order)
+        {
+            if (id != order.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(order);
+                    await _context.SaveChangesAsync();
+                    TempData["Success"] = "Cập nhật thành công";
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!OrderExists(order.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            ViewData["PromotionId"] = new SelectList(_context.Promotions, "Id", "Code", order.PromotionId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", order.UserId);
+            TempData["Error"] = "Cập nhật không thành công";
+            return View(order);
+        }
+
+        // GET: Orders/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Orders == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Orders
+                .Include(o => o.Promotion)
+                .Include(o => o.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return View(order);
+        }
+
+        // POST: Orders/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Orders == null)
+            {
+                return Problem("Entity set 'S2HandDbContext.Orders'  is null.");
+            }
+            var order = await _context.Orders.FindAsync(id);
+            if (order != null)
+            {
+                _context.Orders.Remove(order);
+                await _context.SaveChangesAsync();
+                TempData["Success"] = "Xoá thành công";
+                return RedirectToAction("Index");
+            }
+            TempData["Error"] = "Xoá không thành công";
+            return NotFound();
+        }
+
+        private bool OrderExists(int id)
+        {
+            return (_context.Orders?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
