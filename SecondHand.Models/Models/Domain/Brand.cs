0c3267e74cee22a297b333177bcc54834a60fef3
vule2303 <leductuanvu5@gmail.com>
2023-09-14 10:48:59 +0700

    update-brand

--- SecondHand.Models/Models/Domain/Brand.cs
+++ SecondHand.Models/Models/Domain/Brand.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SecondHand.Models.Domain { 
 
@@ -9,12 +12,15 @@ public partial class Brand
     [Key]
 
     public int Id { get; set; }
-
+    [DisplayName("Tên thương hiệu")]
     public string? Name { get; set; }
 
     public string? Logo { get; set; }
-
+    [DisplayName("Mô tả thương hiệu")]
     public string? Description { get; set; }
+    [NotMapped]
+    [DisplayName("Tải ảnh lên")]
+    public IFormFile? ImageFile { get; set; }
 
     public virtual ICollection<Product> Products { get; set; } = new List<Product>();
 }
