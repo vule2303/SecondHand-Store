b5b53c0c6854c42a3c6584abedb0737bbaaa6b04
vule2303 <leductuanvu5@gmail.com>
2023-09-25 21:51:25 +0700

    add role for user

--- SecondHand.Web/Areas/Admin/Pages/User/Edit.cshtml.cs
+++ SecondHand.Web/Areas/Admin/Pages/User/Edit.cshtml.cs
@@ -3,54 +3,43 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using SecondHand.DataAccess.Migrations;
 using SecondHand.Models.Domain;
 using System.ComponentModel;
 
 namespace MVC_Core.Areas.Admin.Pages.User
 {
-    public class AddRoleModel : PageModel
+    public class EditModel : PageModel
     {
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
-
         private readonly RoleManager<IdentityRole> _roleManager;
-
-        public AddRoleModel(
-            UserManager<ApplicationUser> userManager,
-            SignInManager<ApplicationUser> signInManager,
-            RoleManager<IdentityRole> roleManager)
+        public EditModel(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, RoleManager<IdentityRole> roleManager)
         {
             _userManager = userManager;
+
             _signInManager = signInManager;
             _roleManager = roleManager;
         }
 
-
-
-        [TempData]
-        public string StatusMessage { get; set; }
-
         public ApplicationUser user { get; set; }
-
         [BindProperty]
-   
+        [DisplayName("Các role gán cho user")]
         public string[] RoleNames { get; set; }
 
         public SelectList allRoles { get; set; }
 
-        public async Task<IActionResult> OnGetAsync(string roleid)
+        public async Task<IActionResult> OnGetAsync(string id)
         {
-            if (string.IsNullOrEmpty(roleid))
+            if (string.IsNullOrEmpty(id))
             {
                 return NotFound($"Không có user");
             }
 
-            user = await _userManager.FindByIdAsync(roleid);
+            user = await _userManager.FindByIdAsync(id);
 
             if (user == null)
             {
-                return NotFound($"Không thấy user, id = {roleid}.");
+                return NotFound($"Không thấy user, id = {id}.");
             }
 
             RoleNames = (await _userManager.GetRolesAsync(user)).ToArray<string>();
@@ -65,51 +54,52 @@ namespace MVC_Core.Areas.Admin.Pages.User
             return Page();
         }
 
-        public async Task<IActionResult> OnPostAsync(string roleid)
+        public async Task<IActionResult> OnPostAsync(string id)
         {
-            if (string.IsNullOrEmpty(roleid))
+            if (string.IsNullOrEmpty(id))
             {
                 return NotFound($"Không có user");
             }
 
-            user = await _userManager.FindByIdAsync(roleid);
+            user = await _userManager.FindByIdAsync(id);
 
             if (user == null)
             {
-                return NotFound($"Không thấy user, id = {roleid}.");
+                return NotFound($"Không thấy user, id = {id}.");
             }
 
             // RoleNames
 
             var OldRoleNames = (await _userManager.GetRolesAsync(user)).ToArray();
+
             var deleteRoles = OldRoleNames.Where(r => !RoleNames.Contains(r));
             var addRoles = RoleNames.Where(r => !OldRoleNames.Contains(r));
-             int deleteRolesCount = deleteRoles.Count();
+
             List<string> roleNames = await _roleManager.Roles.Select(r => r.Name).ToListAsync();
             allRoles = new SelectList(roleNames);
-            if(deleteRolesCount != 0)
+
+            var resultDelete = await _userManager.RemoveFromRolesAsync(user, deleteRoles);
+            if (!resultDelete.Succeeded)
             {
-                var resultDelete = await _userManager.RemoveFromRolesAsync(user, deleteRoles);
-                if (!resultDelete.Succeeded)
+                resultDelete.Errors.ToList().ForEach(error =>
                 {
-                    resultDelete.Errors.ToList().ForEach(error => {
-                        ModelState.AddModelError(string.Empty, error.Description);
-                    });
-                    return Page();
-                }
+                    ModelState.AddModelError(string.Empty, error.Description);
+                });
+                return Page();
             }
-            await _userManager.UpdateSecurityStampAsync(user);
+
             var resultAdd = await _userManager.AddToRolesAsync(user, addRoles);
             if (!resultAdd.Succeeded)
             {
-                resultAdd.Errors.ToList().ForEach(error => {
+                resultAdd.Errors.ToList().ForEach(error =>
+                {
                     ModelState.AddModelError(string.Empty, error.Description);
                 });
                 return Page();
             }
 
 
-            StatusMessage = $"Vừa cập nhật role cho user: {user.UserName}";
+
 
             return RedirectToPage("./Index");
         }
