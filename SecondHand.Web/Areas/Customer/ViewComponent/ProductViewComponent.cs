28ab9734d88dd1f77dc33a610c1791de8646c550
ThaiMC <93109563+goddeath008@users.noreply.github.com>
2023-09-25 21:52:23 +0700

    Trang sản phẩm (#81)

---
+++ SecondHand.Web/Areas/Customer/ViewComponent/ProductViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SecondHand.DataAccess.Data;
+using SecondHand.Models.Domain;
+
+namespace MVC_Core.Areas.Customer.ViewComponents
+{
+    public class ProductViewComponent : ViewComponent
+    {
+        private readonly S2HandDbContext _context;
+
+        public ProductViewComponent(S2HandDbContext context) {
+            _context = context;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var a = _context.Products.Where(p => p.Status == true)
+                .Include(p => p.Brand)
+                .Include(p => p.Category)                
+                .Include(p => p.productGallery).ToList();
+
+           
+            return View(a);
+        }
+    }
+}
