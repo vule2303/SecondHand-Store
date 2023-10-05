d0acbda4c94a3de75e748eab57a25b7aba3ececd
ThaiMC <93109563+goddeath008@users.noreply.github.com>
2023-10-05 14:51:01 +0700

    Trang sản phẩm theo danh mục (#94)

---
+++ SecondHand.Web/Areas/Customer/ViewComponent/BranddViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SecondHand.DataAccess.Data;
+
+namespace MVC_Core.Areas.Customer.ViewComponents
+{
+    public class BranddViewComponent : ViewComponent
+    {
+        S2HandDbContext _context = new S2HandDbContext();
+        public IViewComponentResult Invoke()
+        {
+            var pro = _context.Brands.ToList();
+
+            return View(pro);
+        }
+    }
+}
