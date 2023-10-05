e83f0431d7c9edd86f91f6d5c2b437b943cda5f9
your name <congthaifc01@gmail.com>
2023-10-05 16:06:37 +0700

    add ViewModels Procate

---
+++ SecondHand.Models/ViewModels/ProCate.cs
@@ -0,0 +1,18 @@
+﻿using SecondHand.Models.Domain;
+using SecondHand.Models.Models.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondHand.Models.ViewModels
+{
+    public class ProCate
+    {
+        public Product DanhMuc{ get; set; }
+        public Brand ThuongHieu { get; set; }
+      
+        public List<ProductImages> AnhSP { get; set; }
+    }
+}
