be6be1da6bd5e2cad2ff8dbc1d38e68dfa201c3d
your name <congthaifc01@gmail.com>
2023-10-07 02:24:54 +0700

    update product page

--- SecondHand.Web/Areas/Customer/Controllers/FilterController.cs
+++ SecondHand.Web/Areas/Customer/Controllers/FilterController.cs
@@ -1,13 +1,56 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SecondHand.DataAccess.Data;
+using SecondHand.Models.Domain;
+using System.Collections.Generic;
 
 namespace MVC_Core.Areas.Customer.Controllers
 {
     [Area("Customer")]
     public class FilterController : Controller
     {
+        private readonly S2HandDbContext _db;
+
+        public FilterController(S2HandDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+        public IActionResult SPTheoLoai(int loaiSp)
+        {
+            List<Product> lstSp = _db.Products
+                .Include(x=>x.Category)
+                .Include(x=>x.Brand)
+                .Include(x=>x.productGallery)
+                .Where(x=> x.Category.Id == loaiSp)
+                .OrderBy(x=>x.Name).ToList();
+           
+            var aidi = _db.Categories
+                .Where(x=>x.Id == loaiSp).ToList();
+            ViewBag.TenDanhMuc = aidi;
+            return View(lstSp);
+        }
+        public IActionResult SPTheoBrand(int loaiSp)
+        {
+            List<Product> lstSp = _db.Products
+                .Include(x => x.Category)
+                .Include(x => x.Brand)
+                .Include(x => x.productGallery)
+                .Where(x => x.Brand.Id == loaiSp)
+                .OrderBy(x => x.Name).ToList();
+            
+            var aidi = _db.Brands
+                .Where(x => x.Id == loaiSp).ToList();
+            ViewBag.TenDanhMuc = aidi;
+            return View(lstSp);
+        }
+
+
+
+
     }
 }
---
+++ SecondHand.Web/Areas/Customer/ViewComponent/BranddViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Core.Areas.Customer.Repository;
+
+namespace MVC_Core.Areas.Customer.ViewComponents
+{
+    public class BranddViewComponent: ViewComponent
+    {
+        private readonly IBrandRepository _loaiBrand;
+
+        public BranddViewComponent(IBrandRepository brandRepository)
+        {
+            _loaiBrand = brandRepository;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var listSp = _loaiBrand.GetAllLoaiSP().OrderBy(x => x.Id);
+            return View(listSp);
+        }
+    }
+}
