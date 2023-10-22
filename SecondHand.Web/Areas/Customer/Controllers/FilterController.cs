6653b81f322f00daa7d06ae0e00e6ab7d13d270c
Vũ <107258990+vule2303@users.noreply.github.com>
2023-10-22 10:17:16 +0700

    Revert "fix dùm Phân trang sản phẩm  (#114)" (#115)
    This reverts commit 92c8c6a898eaafde99657ab5ba0289c57e77443b.

--- SecondHand.Web/Areas/Customer/Controllers/FilterController.cs
+++ SecondHand.Web/Areas/Customer/Controllers/FilterController.cs
@@ -2,7 +2,6 @@
 using Microsoft.EntityFrameworkCore;
 using SecondHand.DataAccess.Data;
 using SecondHand.Models.Domain;
-using SecondHand.Models.Models.Domain;
 using SecondHand.Models.ViewModels;
 
 
@@ -33,42 +32,24 @@ namespace MVC_Core.Areas.Customer.Controllers
             ViewBag.TenDanhMuc = aidi;
             return View(lstSp);
         }
-        public IActionResult SPTheoBrand(int loaiSp, int pg = 1)
+        public IActionResult SPTheoBrand(int loaiSp)
         {
-            const int pageSize = 1;
-            pg = Math.Max(1, pg);
-
             List<Product> lstSp = _context.Products
                 .Include(x => x.Category)
                 .Include(x => x.Brand)
                 .Include(x => x.productGallery)
                 .Where(x => x.Brand.Id == loaiSp)
-                .OrderBy(x => x.Name)
-                .ToList();
-
-            if (lstSp.Count == 0)
+                .OrderBy(x => x.Name).ToList();
+            if (lstSp == null || lstSp.Count == 0)
             {
                 ViewBag.Empty = "Không có sản phẩm nào!!!";
             }
-
             var aidi = _context.Brands
-                .Where(x => x.Id == loaiSp)
-                .ToList();
-
+                .Where(x => x.Id == loaiSp).ToList();
             ViewBag.TenDanhMuc = aidi;
-
-            var recsCount = lstSp.Count;
-
-            var pager = new Pager(recsCount, pg, pageSize);
-            var recSkip = (pg - 1) * pageSize;
-
-            var data = lstSp.Skip(recSkip).Take(pager.PageSize).ToList();
-
-            ViewBag.Pager = pager;
-
-            return View(data);
+            return View(lstSp);
         }
-
+        
 
     }
 }
