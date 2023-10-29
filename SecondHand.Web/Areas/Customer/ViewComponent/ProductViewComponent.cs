4abf4502a0c602f66bccffb96e1c4e19262e98fb
phu01072003 <nguyenquocphu03nt@gmail.com>
2023-10-29 22:08:24 +0700

    chưa done

--- SecondHand.Web/Areas/Customer/ViewComponent/ProductViewComponent.cs
+++ SecondHand.Web/Areas/Customer/ViewComponent/ProductViewComponent.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using SecondHand.DataAccess.Data;
 using SecondHand.Models.Domain;
 using System.Security.Claims;
+using SecondHand.Models.Models.Domain;
 
 namespace MVC_Core.Areas.Customer.ViewComponents
 {
@@ -16,14 +17,32 @@ namespace MVC_Core.Areas.Customer.ViewComponents
             _context = context;
            
         }
-        public IViewComponentResult Invoke()
+        public IViewComponentResult Invoke(int pg = 1)
         {
             var a = _context.Products.Where(p => p.Status == true)
                 .Include(p => p.Brand)
                 .Include(p => p.Category)                
                 .Include(p => p.productGallery).ToList();
-           
-            return View(a);
+
+            const int pageSize = 8;
+            if (pg < 1)
+                pg = 1;
+
+            int recsCount = a.Count();
+
+            var pager = new Pager(recsCount, pg, pageSize);
+
+            int recSkip = (pg - 1) * pageSize;
+
+            var data = a.Skip(recSkip).Take(pager.PageSize).ToList();
+
+            this.ViewBag.Pager = pager;
+
+            //return View(cateList);
+
+            return View(data);
+
+            //return View(a);
         }
        
     }
